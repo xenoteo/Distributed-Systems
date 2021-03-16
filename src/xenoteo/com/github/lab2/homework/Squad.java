ef973bc4058ef992aa28de8901ce2e36357bf70d
xenoteo <fedorovxa@gmail.com>
2021-03-16 19:04:16 +0100

    lab2 homework refactoring

--- src/xenoteo/com/github/lab2/homework/Squad.java
+++ src/xenoteo/com/github/lab2/homework/Squad.java
@@ -2,63 +2,99 @@ package xenoteo.com.github.lab2.homework;
 
 import com.rabbitmq.client.*;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 import java.util.concurrent.TimeoutException;
 
+/**
+ * The class representing the squad.
+ */
 public class Squad {
-    public static void main(String[] argv) throws IOException, TimeoutException {
-        try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    /**
+     * The name of the squad.
+     */
+    private final String squadName;
 
-            System.out.print("Enter the squad name: ");
-            String squadName = br.readLine();
-            squadName = squadName.toUpperCase();
-            System.out.printf("SQUAD %s\n", squadName);
+    public Squad() {
+        squadName = readSquadName();
+    }
 
+    public static void main(String[] argv) {
+        Squad squad = new Squad();
+        squad.run();
+    }
+
+    /**
+     * Runs the process of sending requests to suppliers and receiving confirmation from them.
+     */
+    private void run(){
+        System.out.printf("SQUAD %s\n", squadName);
+        try {
+            // setting up the connection & channel
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost("localhost");
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
 
-            String EXCHANGE_NAME = "tourism";
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+            // setting up the exchange
+            channel.exchangeDeclare(Supplier.EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
 
+            // setting up the queue
             channel.queueDeclare(squadName, false, false, false, null);
 
-            Consumer consumer = new DefaultConsumer(channel) {
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
-                    String message = new String(body, StandardCharsets.UTF_8);
-                    String confirmation = String.format("Received %s", message);
-                    ColoredOutput.printlnGreen(confirmation);
-                }
-            };
-
+            // creating the consumer for receiving confirmations and starting receiving them
+            Consumer consumer = createSquadConsumer(channel);
             channel.basicConsume(squadName, true, consumer);
 
+            Scanner in = new Scanner(System.in);
             while (true) {
+                // reading items to supply from terminal
                 System.out.println("Enter item to supply (q to quit):");
-                String item = br.readLine();
-
+                String item = in.nextLine().toLowerCase();
                 if ("q".equals(item)) {
                     break;
                 }
 
-                item = item.toLowerCase();
+                // creating the queue for sending the request and sending the request
                 channel.queueDeclare(item, false, false, false, null);
-
                 channel.basicPublish("", item, null, squadName.getBytes(StandardCharsets.UTF_8));
+
                 System.out.printf("Sent request for %s\n", item);
             }
 
             channel.close();
             connection.close();
-
-        } catch (TimeoutException | IOException e) {
+        } catch (IOException | TimeoutException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * Reads the name of the squad from terminal.
+     *
+     * @return the name of the squad
+     */
+    private String readSquadName(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the squad name: ");
+        return in.nextLine().toUpperCase();
+    }
+
+    /**
+     * Creates a consumer for receiving confirmations from suppliers by the squad.
+     *
+     * @param channel  the channel
+     * @return the squad consumer
+     */
+    private Consumer createSquadConsumer(Channel channel){
+        return new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+                String message = new String(body, StandardCharsets.UTF_8);
+                String confirmation = String.format("Received %s", message);
+                ColoredOutput.printlnGreen(confirmation);
+            }
+        };
     }
 }
--- src/xenoteo/com/github/lab2/homework/Supplier.java
+++ src/xenoteo/com/github/lab2/homework/Supplier.java
@@ -2,88 +2,147 @@ package xenoteo.com.github.lab2.homework;
 
 import com.rabbitmq.client.*;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * The class representing the supplier.
+ */
 public class Supplier {
-    public static int id = 0;
+    /**
+     * The exchange name.
+     */
+    public static final String EXCHANGE_NAME = "tourism";
+    /**
+     * The number of the last order handled by the supplier.
+     */
+    private long lastCommissionNumber;
+    /**
+     * The name of the supplier.
+     */
+    private final String supplierName;
+    /**
+     * The supplier prefix.
+     */
+    private final String supplierPrefix;
+
+    public Supplier() {
+        lastCommissionNumber = 0;
+        supplierName = readSupplierName();
+        supplierPrefix = supplierName
+                .replaceAll("\\p{Punct}", "").replaceAll("\\s", "") + "-";
+        System.out.println(supplierPrefix);
+    }
+
     public static void main(String[] argv) {
-        AtomicLong lastCommissionNumber = new AtomicLong(0);
+        Supplier supplier = new Supplier();
+        supplier.run();
+    }
 
-        id++;
-        System.out.printf("SUPPLIER %d\n", id);
-
-        Set<String> equipment = readEquipment();
-
+    /**
+     * Runs the process of receiving requests from squads, handling requests and sending the confirmations to squads.
+     */
+    private void run(){
+        System.out.printf("SUPPLIER %s\n", supplierName);
         try{
+            // setting up the connection & channel
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost("localhost");
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
+
+            // setting up the QoS
             channel.basicQos(1);
 
-            String EXCHANGE_NAME = "tourism";
+            // setting up the exchange
             channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
 
-            assert equipment != null;
-            for (String item : equipment){
-                channel.queueDeclare(item, false, false, false, null);
-            }
+            // creating the consumer for receiving and handling requests from squads and sending the confirmations
+            Consumer consumer = createSupplierConsumer(channel);
 
-            Consumer consumer = new DefaultConsumer(channel) {
+            // reading the equipment supplied by the supplier
+            Set<String> equipment = readEquipment();
 
-                @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
-                    try{
-                        String message = new String(body, StandardCharsets.UTF_8);
-                        String item = envelope.getRoutingKey();
-                        System.out.printf("Received request for %s from %s\n", item, message);
-
-                        long commissionNumber = lastCommissionNumber.incrementAndGet();
-                        String confirmation = String.format("%s (commission #%d)", item, commissionNumber);
-
-                        channel.queueDeclare(message, false, false, false, null);
-                        channel.basicPublish("", message, null, confirmation.getBytes(StandardCharsets.UTF_8));
-
-                        channel.basicAck(envelope.getDeliveryTag(), false);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            };
-
+            // start receiving requests on each equipment item's channel
             for (String queueName : equipment){
+                channel.queueDeclare(queueName, false, false, false, null);
                 channel.basicConsume(queueName, false, consumer);
             }
-        } catch (TimeoutException | IOException e) {
+        } catch (IOException | TimeoutException e) {
             e.printStackTrace();
         }
-
     }
 
-    private static Set<String> readEquipment(){
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            Set<String> equipment = new HashSet<>();
-            System.out.println("Enter equipment supplied (s to stop):");
-            String item = br.readLine().toLowerCase();
-            while (!item.equals("s")){
-                if (!item.isEmpty())
-                    equipment.add(item);
-                item = br.readLine().toLowerCase();
+    /**
+     * Reads the name of the supplier from terminal.
+     *
+     * @return the name of the supplier
+     */
+    private String readSupplierName(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the supplier name: ");
+        return in.nextLine().toUpperCase();
+    }
+
+    /**
+     * Creates a consumer for receiving requests from squads, handling requests and sending the confirmations to squads.
+     *
+     * @param channel  the channel
+     * @return the supplier consumer
+     */
+    private Consumer createSupplierConsumer(Channel channel){
+        return new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) {
+                try{
+                    // getting the name of the squad and the item needed
+                    String squad = new String(body, StandardCharsets.UTF_8);
+                    String item = envelope.getRoutingKey();
+                    System.out.printf("Received request for %s from %s\n", item, squad);
+
+                    // creating the confirmation
+                    String commissionNumber = supplierPrefix + (++lastCommissionNumber);
+                    String confirmation = String.format("%s (commission %s)", item, commissionNumber);
+
+                    // creating the queue for sending the confirmation to the squad and sending it
+                    channel.queueDeclare(squad, false, false, false, null);
+                    channel.basicPublish("", squad, null, confirmation.getBytes(StandardCharsets.UTF_8));
+
+                    // confirming the request
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
-            System.out.printf("Supplying the following equipment: %s\n", equipment.toString());
-            return equipment;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+        };
     }
+
+    /**
+     * Reads the equipment supplied by the supplier.
+     *
+     * @return the equipment supplied by the supplier
+     */
+    private Set<String> readEquipment(){
+        Scanner in = new Scanner(System.in);
+        Set<String> equipment = new HashSet<>();
+
+        System.out.println("Enter equipment supplied (s to stop):");
+
+        String item = in.nextLine().toLowerCase();
+        while (!item.equals("s")){
+            if (!item.isEmpty())
+                equipment.add(item);
+            item = in.nextLine().toLowerCase();
+        }
+
+        System.out.printf("Supplying the following equipment: %s\n", equipment.toString());
+        return equipment;
+    }
+
+
 }
